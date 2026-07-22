52dbb9af2c73a6471d11f24f1aebf2c9a57acf3f
Aiminnn <136608699+Aimin-0111@users.noreply.github.com>
2026-07-22 10:29:38 -0400

    Implement Fuel_Price_Fixing function
    Added Fuel_Price_Fixing function to convert Fuel_Price column to float.

--- main.py
+++ main.py
@@ -14,6 +14,18 @@ url = "https://drive.google.com/uc?id=1u37-Uh9xNIE8UCMiPq8iNHCBqyiagVR3"
 gdown.download(url, "data.csv", quiet=False)
 df = pd.read_csv("data.csv")
 
+def Fuel_Price_Fixing(df): #Fxing the Fuel_Price column to remove the currency unit and convert it to float
+    def unit_fixer(original): #defining a function to fix the unit of the Fuel_Price column
+        split_arr = str(original).split(" ")
+        if len(split_arr) > 1 and split_arr[0] == "CAD":
+            return float(split_arr[1])
+        else:
+            return float(split_arr[0])
+        
+    df['Fuel_Price'] = df['Fuel_Price'].apply(lambda x: unit_fixer(x))
+    '''df['Fuel_Price'] = df['Fuel_Price'].apply(lambda x: type(x))''' #Testing the type of the Fuel_Price column after applying the unit_fixer function
+    return df
+  
 def DataTesting():
   
 
