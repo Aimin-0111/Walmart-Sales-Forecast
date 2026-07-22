413ef2788d7ce1d41b2ef3121a40bea6d9bb923e
Aiminnn <136608699+Aimin-0111@users.noreply.github.com>
2026-07-22 11:39:04 -0400

    Fix Fuel_Price column conversion and handle missing values
    Fix Fuel_Price column by converting to float and filling missing values with median.

--- main.py
+++ main.py
@@ -30,6 +30,7 @@ def IsHoliday_Fixing(df):
   return(df)
     
 def Fuel_Price_Fixing(df): #Fxing the Fuel_Price column to remove the currency unit and convert it to float
+    
     def unit_fixer(original): #defining a function to fix the unit of the Fuel_Price column
         split_arr = str(original).split(" ")
         if len(split_arr) > 1 and split_arr[0] == "CAD":
@@ -39,6 +40,8 @@ def Fuel_Price_Fixing(df): #Fxing the Fuel_Price column to remove the currency u
         
     df['Fuel_Price'] = df['Fuel_Price'].apply(lambda x: unit_fixer(x))
     '''df['Fuel_Price'] = df['Fuel_Price'].apply(lambda x: type(x))''' #Testing the type of the Fuel_Price column after applying the unit_fixer function
+    median_fuel_price = df['Fuel_Price'].median() # calculating the median
+    df['Fuel_Price'] = df['Fuel_Price'].fillna(median_fuel_price) # replacing missing values without chained inplace assignment
     return df
   
 def DataTesting():
